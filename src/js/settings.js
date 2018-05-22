532f2f063ff0961045113c922672270eef01c94e
Иван Питенин <ivan.pitenin@gmail.com>
2018-05-23 00:40:00 +0300

    start/finish index.html

---
+++ src/js/settings.js
@@ -0,0 +1,34 @@
+$('.owl-carousel.slider__main').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
+$('.owl-carousel.gallery__main').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 3
+    }
+  }
+});
